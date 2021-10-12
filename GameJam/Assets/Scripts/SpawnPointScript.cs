ae02234d866cb63cb7ed38680304030748f8f839
Berk Şengül <31830959+Pyth0x@users.noreply.github.com>
2021-10-12 15:00:29 +0300

    enemySpawn

--- GameJam/Assets/Scripts/SpawnPointScript.cs
+++ GameJam/Assets/Scripts/SpawnPointScript.cs
@@ -1,32 +1,77 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Enemy{
+
+    public string name; 
+
+    public GameObject prefab;
+    [Range(0f, 100f)] public float chance = 100f;
+    [HideInInspector] public float _weight;
+
+}
+
 public class SpawnPointScript : MonoBehaviour
 {
-    public GameObject[] enemies;
-    public float enemySpawnTime = 0.002f;
-    int[] spawnPoints = { -3, 0, 3 };
 
-    void Start()
+    [SerializeField] private Enemy[] enemies;
+    public float enemySpawnTime = 0.002f;
+    
+    System.Random rand = new System.Random();
+    int[] spawnPoints = { -3, 0, 3 };
+    float accumulatedWeights;
+
+    private void Awake()
     {
-        InvokeRepeating("spawnEnemies", 0, enemySpawnTime);
+        CalculateWeights();
     }
 
+    void Start()
+   {
+       InvokeRepeating("spawnEnemies", 0, enemySpawnTime);
+   }
 
-    void spawnEnemies()
+
+   void spawnEnemies()
+   {
+        int randomSpawnPoint = spawnPoints[GetRandomEnemyIndex()];
+        Enemy randomEnemy = enemies[GetRandomEnemyIndex()];
+        Debug.Log(randomEnemy.name + randomEnemy.chance);
+
+
+        if (randomEnemy.prefab.name == "Enemy Capsule")
+       {
+           Vector3 vector = new Vector3(randomSpawnPoint, transform.position.y+0.5f, transform.position.z);
+           Instantiate(randomEnemy.prefab, vector, Quaternion.identity);
+       }
+       else
+       {
+           Vector3 vector = new Vector3(randomSpawnPoint, transform.position.y, transform.position.z);
+           Instantiate(randomEnemy.prefab, vector, Quaternion.identity);
+       }
+
+    }
+
+    int GetRandomEnemyIndex()
     {
-        int randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-        GameObject randomEnemy = enemies[Random.Range(0, enemies.Length)];
-        if(randomEnemy.gameObject.name == "Enemy Capsule")
+        double r = rand.NextDouble() * accumulatedWeights;
+
+        for (int i = 0; i < enemies.Length; i++)
+            if (enemies[i]._weight >= r)
+                return i;
+
+        return 0;
+    }
+
+    void CalculateWeights()
+    {
+        accumulatedWeights = 0f;
+        foreach(Enemy enemy in enemies)
         {
-            Vector3 vector = new Vector3(randomSpawnPoint, transform.position.y+0.5f, transform.position.z);
-            Instantiate(randomEnemy, vector, Quaternion.identity);
-        }
-        else
-        {
-            Vector3 vector = new Vector3(randomSpawnPoint, transform.position.y, transform.position.z);
-            Instantiate(randomEnemy, vector, Quaternion.identity);
+            accumulatedWeights += enemy.chance;
+            enemy._weight = accumulatedWeights;
         }
     }
 }
